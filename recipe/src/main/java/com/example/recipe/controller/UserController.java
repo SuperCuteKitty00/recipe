1fa24eb5b3a0afc35a11170dd18b46d0fc63163e
이현 <nx2028@gmail.com>
2024-05-29 13:45:08 +0900

    레시피 관련 controller, repository, service 클래스 재작업

---
+++ recipe/src/main/java/com/example/recipe/controller/UserController.java
@@ -0,0 +1,11 @@
+package com.example.recipe.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+
+@Controller
+@RequiredArgsConstructor
+public class UserController {
+    /*회원가입, 로그인 관련 컨트롤러 여기 작성해주시면 됩니다. repository, service, dto등
+    User 관련 패키지들 이름은 UserRepository와 같이 작성해주세요.*/
+}
--- recipe/src/main/java/com/example/recipe/service/RecipeService.java
+++ recipe/src/main/java/com/example/recipe/service/RecipeService.java
@@ -1,12 +1,28 @@
 package com.example.recipe.service;
 
+import com.example.recipe.entity.RecipeInfo;
+import com.example.recipe.repository.RecipeInfoRepository;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.io.IOException;
 
 @Service
 public class RecipeService {
 
+    @Autowired
+    private RecipeInfoRepository recipeInfoRepository;
+
+    private RecipeInfo saveRecipe(RecipeInfo recipeInfo){
+            return recipeInfoRepository.save(recipeInfo);
+    }
+
     public String transformPartsDetails(String rcpPartsDtls) {
-        /* 재료 데이터 변환 로직 */
+        /* 재료 데이터 변환 로직
+           예시 데이터: "연두부 75g(3/4모), 칵테일새우 20g(5마리), 달걀 30g(1/2개), 생크림 13g(1큰술), 설탕 5g(1작은술), 무염버터 5g(1작은술) 고명 시금치 10g(3줄기)"
+           변환 후: "연두부, 칵테일새우, 달걀, 생크림, 설탕, 무염버터, 고명 시금치" */
         return rcpPartsDtls.replaceAll("\\s?\\d+\\w*\\([^)]*\\)", "").replaceAll(",\\s+", ",").replaceAll("\\s+", " ");
     }
 
